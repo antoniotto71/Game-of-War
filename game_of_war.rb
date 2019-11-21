5e04025cdaac26e24526cff8cc4af091f3acc124
antoniotto71 <antoniotto71@yahoo.it>
2019-11-21 21:56:05 +0100

    Added initializer

--- game_of_war.rb
+++ game_of_war.rb
@@ -22,9 +22,29 @@ STRINGHE_VALORE = {
   cinque: "5",
   sei: "6",
   sette: "7",
-  otto: "8"
+  otto: "8",
+  nove: "9",
+  dieci: "10",
+  fante: "J",
+  regina: "Q",
+  re: "K",
+  asso: "A"
+}
 
-
+VALORE_DI_GUERRA = {
+  due: 2,
+  tre: 3,
+  quattro: 4,
+  cinque: 5,
+  sei: 6,
+  sette: 7,
+  otto: 8,
+  nove: 9,
+  dieci: 10,
+  fante: 11,
+  regina: 12,
+  re: 13,
+  asso: 14
 }
 
 # Restituisce un array di tutti i semi.
@@ -32,14 +52,28 @@ def self.semi
   STRINGHE_SEME.keys
 end
 
-# Restituisce un array di tutti i valori.each do |
-|
+# Restituisce un array di tutti i valori.
+def self.valori
+  STRINGHE_VALORE.keys
+end
 
+attr_reader :seme, :valore
+
+def initialize(seme, valore)
+  unless Carta.semi.include?(seme) and Carta.valori.include?(valore)
+    raise "seme (#{seme}) o valore (#{valore}) non permesso"
+  end
+
+  @seme, @valore = seme, valore
+end
 
 end
 
 
+#c = Carta.new("cuori", "dieci")
 
-end
+#puts c
 
-puts Carta.semi
+puts Carta.semi.class
+
+puts Carta.valori
